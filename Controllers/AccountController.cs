4f2bd5cd4ed6d78d4f964fbb77ab0df77c25e62d
ilazil <li70534@ubt-uni.net>
2026-01-15 21:53:03 +0100

    Updates
    Some Visual fixes and Profile View

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WorkChat2.Models;
+using WorkChat2.ViewModels;
+
+namespace WorkChat2.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        public AccountController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Profile()
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            if (user == null) return NotFound();
+
+            var vm = new ProfileVm
+            {
+                Name = user.Name,
+                LastName = user.LastName,
+                Email = user.Email,
+                UserName = user.UserName,
+                CreatedAt = user.CreatedAt
+            };
+            return View(vm);
+        }
+    }
+}
---
+++ ViewModels/ProfileVm.cs
@@ -0,0 +1,12 @@
+﻿
+namespace WorkChat2.ViewModels
+{
+    public class ProfileVm
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
